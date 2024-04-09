4f02a37fb280912c9504eba8ce4dbdbe23232360
Steve Schwab <schwabs@umich.edu>
2024-04-09 12:08:45 -0700

    complete `renders texts and alt texts correctly` test

--- frontend/components/tests/Episode.test.js
+++ frontend/components/tests/Episode.test.js
@@ -32,23 +32,41 @@ describe('Episode component', () => {
   })
   test("renders texts and alt texts correctly", () => {
     // 👉 TASK: render the component passing episode data and getting the rerender utility
-
+    const { rerender, container } = render(<Episode episode={exampleEpisodeData} />);
     // 👉 TASK: check that the summary renders to the DOM
-
+    expect(screen.queryByText(/A young boy mysteriously disappears/i)).toBeInTheDocument();
     // 👉 TASK: check that the alt text "episode image" is present
-
+    expect(screen.getByAltText("episode image")).toBeInTheDocument();
     // 👉 TASK: rerender the component passing episode data lacking an image
     // ❗ Study the Episode component to understand what happens in this case
-
+    rerender(<Episode episode={
+      {
+        airdate: "2016-07-15",
+        airstamp: "2016-07-15T12:00:00+00:00",
+        airtime: "",
+        id: 553946,
+        image: "",
+        name: "Chapter One: The Vanishing of Will Byers",
+        number: 1,
+        rating: { average: 8.2 },
+        runtime: 49,
+        season: 1,
+        summary: "A young boy mysteriously disappears, and his panicked mother \
+      demands that the police find him. Meanwhile, the boy's friends conduct \
+      their own search, and meet a mysterious girl in the forest.",
+        type: "regular",
+        url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+      }
+    } />)
     // 👉 TASK: check that the default image appears in the DOM
     // ❗ Use querySelector to select the image by its src attribute
-
+    expect(container.querySelector("img[src='https://i.ibb.co/2FsfXqM/stranger-things.png']"));
     // 👉 TASK: check that the "generic episode image" alt text is present
-
+    expect(screen.getByAltText("generic episode image")).toBeInTheDocument();
     // 👉 TASK: rerender the component passing an undefined episode
     // ❗ Study the Episode component to understand what happens in this case
-
+    rerender(<Episode episode={undefined} />);
     // 👉 TASK: check that the "Loading episode..." text is present
-
+    expect(screen.queryByText('Loading episode...')).toBeInTheDocument();
   })
 })
